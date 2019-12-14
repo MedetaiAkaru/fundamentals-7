89f8117adda328f4563ea99724a786a2606f1fe8
Joel Lim <akaru@littlix.com>
2019-12-14 23:56:26 +0800

    regex

---
+++ exercise_50.rb
@@ -0,0 +1,11 @@
+# Based on your code for Exercise 10, create a Day Organiser
+
+# First, ask for a time as a single input string in military time (e.g. “16:30”) or standard time (e.g. 04:30PM)
+# Then, ask for the activity that will be happening during that time as a string
+# Ask the user if that is the last activity they would like to input (Y/N)
+# If it’s the last activity, then, list out in order of the day, the activity of the day, together with the time in standard time.
+# e.g. Your output should be:
+# Your activities today are:
+# 04:30AM - Wake up, eat breakfast
+# 05:30AM - Go to the gym
+# 08:00AM - Head for Recode Class
